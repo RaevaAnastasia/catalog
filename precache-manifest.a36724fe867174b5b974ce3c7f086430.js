fe910afc030248f263c308f9da830edf09f34fb0
Anastasia Raeva <anastassia.raeva@gmail.com>
2020-02-24 23:26:48 +0300

    Updates

---
+++ precache-manifest.a36724fe867174b5b974ce3c7f086430.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bea81e9988e17b73253d6221e6d93c",
+    "url": "/catalog/index.html"
+  },
+  {
+    "revision": "4148a883e5cd6ca85974",
+    "url": "/catalog/static/css/main.914ba867.chunk.css"
+  },
+  {
+    "revision": "2266f31694a36ba2b7b3",
+    "url": "/catalog/static/js/2.07b23a7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/catalog/static/js/2.07b23a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4148a883e5cd6ca85974",
+    "url": "/catalog/static/js/main.4075432e.chunk.js"
+  },
+  {
+    "revision": "3af36022912eb50c011d",
+    "url": "/catalog/static/js/runtime-main.b9e25166.js"
+  },
+  {
+    "revision": "b1ca844626ec2c1af3e4b1a00b743e9a",
+    "url": "/catalog/static/media/cart.b1ca8446.svg"
+  },
+  {
+    "revision": "cb308f98b0c854a74b495af9dfa83859",
+    "url": "/catalog/static/media/product-1.cb308f98.jpg"
+  },
+  {
+    "revision": "91f371f22ba624efd791fa170dc767d3",
+    "url": "/catalog/static/media/product-10.91f371f2.jpg"
+  },
+  {
+    "revision": "22e8aefa0bc4d4c3d7a1fcc4f312db3a",
+    "url": "/catalog/static/media/product-11.22e8aefa.jpg"
+  },
+  {
+    "revision": "3133f1e4181010a0554177385a79d3be",
+    "url": "/catalog/static/media/product-12.3133f1e4.jpg"
+  },
+  {
+    "revision": "1dea6dfb04166fe76e291de0086e5bd4",
+    "url": "/catalog/static/media/product-13.1dea6dfb.jpg"
+  },
+  {
+    "revision": "dfe4c371f5c29308c4b108eb690249f1",
+    "url": "/catalog/static/media/product-14.dfe4c371.jpg"
+  },
+  {
+    "revision": "36ee41b18856feec6dd5a050c85cb46a",
+    "url": "/catalog/static/media/product-15.36ee41b1.jpg"
+  },
+  {
+    "revision": "b2279288900a83d25df13c1bba57fee0",
+    "url": "/catalog/static/media/product-16.b2279288.jpg"
+  },
+  {
+    "revision": "98884be269381883662b17abcaf126ac",
+    "url": "/catalog/static/media/product-17.98884be2.jpg"
+  },
+  {
+    "revision": "8fa5d8fdb838a5bc3d709d6d35369ed7",
+    "url": "/catalog/static/media/product-18.8fa5d8fd.jpg"
+  },
+  {
+    "revision": "a074ca7eec4c88d35ac08abb866575d0",
+    "url": "/catalog/static/media/product-19.a074ca7e.jpg"
+  },
+  {
+    "revision": "8059dafd9bab6b36c7b1c2e2f015c4d4",
+    "url": "/catalog/static/media/product-2.8059dafd.jpg"
+  },
+  {
+    "revision": "84320634573bb2eca98d2dae86201211",
+    "url": "/catalog/static/media/product-20.84320634.jpg"
+  },
+  {
+    "revision": "ab4efc127c24fa03bf180e9b8243aa23",
+    "url": "/catalog/static/media/product-21.ab4efc12.jpg"
+  },
+  {
+    "revision": "812e9e61498f119be6a061d41e503f3f",
+    "url": "/catalog/static/media/product-22.812e9e61.jpg"
+  },
+  {
+    "revision": "320992c155abcbd5cb5bd0ea0f4c774c",
+    "url": "/catalog/static/media/product-23.320992c1.jpg"
+  },
+  {
+    "revision": "b8b43134b1582655b212f4453c846a6b",
+    "url": "/catalog/static/media/product-24.b8b43134.jpg"
+  },
+  {
+    "revision": "47a275d50334c4aee5c124a9cbd61814",
+    "url": "/catalog/static/media/product-25.47a275d5.jpg"
+  },
+  {
+    "revision": "badd67490dd068dfadc6e3b3a82a4011",
+    "url": "/catalog/static/media/product-3.badd6749.jpg"
+  },
+  {
+    "revision": "4824ac8d935b9250916f740d4b106ad8",
+    "url": "/catalog/static/media/product-4.4824ac8d.jpg"
+  },
+  {
+    "revision": "14b9f8d21fba99eb409252a550c47d0e",
+    "url": "/catalog/static/media/product-5.14b9f8d2.jpg"
+  },
+  {
+    "revision": "2757d20b01bae3fb32dcb41fa1846a29",
+    "url": "/catalog/static/media/product-6.2757d20b.jpg"
+  },
+  {
+    "revision": "44482d6755b6080105002ea9c37650b8",
+    "url": "/catalog/static/media/product-7.44482d67.jpg"
+  },
+  {
+    "revision": "d03993c5159dec40aaf53d05d699226c",
+    "url": "/catalog/static/media/product-8.d03993c5.jpg"
+  },
+  {
+    "revision": "105314b2e6bcbed62de62371f090e504",
+    "url": "/catalog/static/media/product-9.105314b2.jpg"
+  }
+]);
